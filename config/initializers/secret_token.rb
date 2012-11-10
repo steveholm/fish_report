c99cfc7988a45d2cd643adab8aa8002ebf4db2b3
Steve Holm <steveholmphoto@me.com>
2012-11-09 20:45:25 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FishReport::Application.config.secret_token = '6db708c77baa6d269722754f4a794f5d67ee66eda95832346159cab530e185686830a94c3986e97832cd8cea07ef9c746bc0a6e407010a20c14ed603e01aecf3'
