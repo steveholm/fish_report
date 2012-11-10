703e9e1b79506cd43e372fd64bc3940ada5896c5
Steve Holm <steveholmphoto@me.com>
2012-11-10 15:39:15 -0600

    add styles and stuff

---
+++ app/helpers/fish_strategy_helper.rb
@@ -0,0 +1,23 @@
+module FishStrategyHelper
+
+
+# 	def weather_condition_class_img(current_weather)
+	    
+# 	    if current_observation.condition.downcase.match('mostly cloudy')
+# 	      return "body-bg-rainy"
+# 	    elsif current_weather.condition.downcase.match('sun') || current_weather.condition.downcase.match('cloud')
+# 	      return "/assets/backgrounds/sunny-bg.jpg"
+# 	    elsif current_weather.condition.downcase.match('snow')
+# 	      return "/assets/backgrounds/snowing-bg.jpg"
+# 	    elsif current_weather.condition.downcase.match('overcast')
+# 	      return "/assets/backgrounds/overcast-bg.jpg"
+# 	    elsif current_weather.condition.downcase.match('clear')
+# 	      return "/assets/backgrounds/clear-night-bg.jpg"
+# 	    # else
+# 	      # return "sunny"
+# 	    end
+# 	end
+
+end
+
+# "current_observation"]["weather"]
